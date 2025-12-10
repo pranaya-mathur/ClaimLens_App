af1080f21f81afef9e94d26b033de92bf1786b1e
Pranay <pranaya.mathur@yahoo.com>
2025-12-10 09:39:06 +0530

    Update Neo4j password to claimlens123

--- api/routes/ingest.py
+++ api/routes/ingest.py
@@ -17,7 +17,7 @@ router = APIRouter()
 # Neo4j connection from environment
 NEO4J_URI = os.getenv("NEO4J_URI", "bolt://localhost:7687")
 NEO4J_USER = os.getenv("NEO4J_USER", "neo4j")
-NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "neo4j")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD", "claimlens123")
 
 # Global ingestor instance (connection pooling)
 ingestor = None
