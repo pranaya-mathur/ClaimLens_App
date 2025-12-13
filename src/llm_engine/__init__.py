a5f21aa64abbe1796534fe1bb1e308a5a0032756
Pranay <pranaya.mathur@yahoo.com>
2025-12-13 18:59:59 +0530

    Add LLM engine module initialization

---
+++ src/llm_engine/__init__.py
@@ -0,0 +1,6 @@
+"""LLM Engine for Semantic Aggregation and Explanations"""
+
+from .semantic_aggregator import SemanticAggregator
+from .explanation_generator import ExplanationGenerator
+
+__all__ = ["SemanticAggregator", "ExplanationGenerator"]
